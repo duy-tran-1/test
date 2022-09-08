66a0a84f9160b30f42817575f854cb311e24f478
duy.tran52 <duy.tran52@tafensw.edu.au>
2022-09-08 15:15:55 +0930

    Send back object

--- src/main/java/com/example/demo/controller/HelloController.java
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -1,7 +1,13 @@
-package com.example.demo.HelloController.java;
+package com.example.demo.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import com.example.demo.model.Greeting;
 
 @RestController
 public class HelloController {
@@ -9,6 +15,14 @@ public class HelloController {
     @GetMapping("/")
     public String index() {
         return "Hello World!";
+    }
+
+    @GetMapping(value = "/greeting", produces = {MediaType.APPLICATION_JSON_VALUE})
+    @CrossOrigin(origins = "http://localhost:8000")
+    @ResponseBody
+    public ResponseEntity<Greeting> greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+        Greeting greeting = new Greeting(1,String.format("Hello, %s!", name));
+        return ResponseEntity.ok().body(greeting);
     }
 
 }
